ae721725d8e47363d96774e7199ad94686538b4b
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-09 16:54:24 +0100

    fix scroll behavior

--- src/components/ResumeLayout.vue
+++ src/components/ResumeLayout.vue
@@ -8,7 +8,7 @@
       @toggleFilter="toggleFilter"
     />
 
-    <div class="content">
+    <div class="content" id="content">
       <WorkExperience
         :work="work"
         :activeFilters="activeFilters"
